eef2fd5ec43a29b3f4e83cde54132bcaaad858bf
zedshaw <zedshaw>
2010-04-27 14:45:02 +0000

    Ex 9 and 10.

--- ex/ex10.py
+++ ex/ex10.py
@@ -1,1 +1,15 @@
-# empty exercise
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ an \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t*Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
--- ex/ex9.py
+++ ex/ex9.py
@@ -1,13 +1,10 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan Feb Mar Apr May Jun Jul Aug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here's the days: ", days
 print "Here's the months: ", months
-
-print days.split(' ')  # that's a space
-print months.split(' ')  # any idea what that did?
 
 print """
 There's something going on here.
