c0e0e75324a20ab8d34d804c056e4ffe1b63fee2
zedshaw <zedshaw>
2010-04-28 05:10:03 +0000

    Exercise 12 done.

--- ex/ex12.py
+++ ex/ex12.py
@@ -1,1 +1,7 @@
-# empty exercise
+age = raw_input("How old are you? ")
+height = raw_input("How many tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
