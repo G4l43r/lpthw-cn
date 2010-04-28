c82e26a98cc5d30abe3c1cf0ed716753fd5a759e
zedshaw <zedshaw>
2010-04-28 03:42:55 +0000

    Fixes for typos in ch10.

--- ex/ex10.py
+++ ex/ex10.py
@@ -1,12 +1,12 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ an \\ cat."
+backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
 I'll do a list:
 \t* Cat food
 \t* Fishies
-\t* Catnip\n\t*Grass
+\t* Catnip\n\t* Grass
 """
 
 print tabby_cat
