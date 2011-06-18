b2deb2a60cff1874e1e8f8cf44d74fca23289bae
Zed A. Shaw <zedshaw@zedshaw.com>
2011-06-18 12:23:46 -0700

    Better ex 51 explanation, fitting with the current style.

---
+++ ex/ex51/gothonweb/form_test.py
@@ -0,0 +1,21 @@
+import web
+
+urls = (
+  '/hello', 'index'
+)
+
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class index:
+    def GET(self):
+        form = web.input(name="Nobody")
+        greeting = "Hello, %s" % form.name
+
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
+
