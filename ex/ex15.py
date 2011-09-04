5d6419a948a172dd9c50087206d9aa0ca6634712
Zed A. Shaw <zedshaw@zedshaw.com>
2011-09-04 14:51:06 -0700

    Clearer way to say the same thing.

--- ex/ex15.py
+++ ex/ex15.py
@@ -7,7 +7,7 @@ txt = open(filename)
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "I'll also ask you to type it again:"
+print "Type the filename again:"
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
